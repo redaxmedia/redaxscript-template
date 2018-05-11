a34216cf2b0e71db8ed56602ed083bed5d0cdde8
redaxmedia <info@redaxmedia.com>
2018-05-11 23:09:30 +0200

    Update dependencies

--- gruntfile.js
+++ gruntfile.js
@@ -2,20 +2,18 @@ module.exports = function (grunt)
 {
 	'use strict';
 
-	grunt.path = require('path');
-
 	/* config grunt */
 
 	grunt.initConfig(
 	{
+		htmlhint: require('./tasks/htmlhint')(grunt),
 		jsonlint: require('./tasks/jsonlint')(grunt),
 		ncsslint: require('./tasks/ncsslint')(grunt),
-		htmlhint: require('./tasks/htmlhint')(grunt),
 		postcss: require('./tasks/postcss')(grunt),
-		tocgen: require('./tasks/tocgen')(grunt),
-		webfont: require('./tasks/webfont')(grunt),
 		rename: require('./tasks/rename')(grunt),
-		svgmin: require('./tasks/svgmin')(grunt)
+		svgmin: require('./tasks/svgmin')(grunt),
+		tocgen: require('./tasks/tocgen')(grunt),
+		webfont: require('./tasks/webfont')(grunt)
 	});
 
 	/* load tasks */

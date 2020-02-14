8bd7a232edc7b82d40726c81e82a2e10c811f12c
redaxmedia <info@redaxmedia.com>
2020-02-15 00:57:54 +0100

    Improve typo of home buttons

--- templates/redaxscript/helper/quick-start.php
+++ templates/redaxscript/helper/quick-start.php
@@ -8,6 +8,8 @@ $doc = new DOMDocument();
 $doc->loadHTML(Db::forTablePrefix('articles')->where('alias', 'quick-start-1001')->findOne()->text);
 $xpath = new DOMXPath($doc);
 
+/* process nodes */
+
 foreach ($xpath->query('//a') as $node)
 {
 	$node->parentNode->removeChild($node);

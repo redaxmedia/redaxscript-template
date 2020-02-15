5b472dda086af0e8576f8c55b55770e2b186bfbd
redaxmedia <info@redaxmedia.com>
2020-02-15 13:11:26 +0100

    Fix doctype thing

--- templates/redaxscript/helper/quick-start.php
+++ templates/redaxscript/helper/quick-start.php
@@ -5,7 +5,7 @@ use DOMDocument;
 use DOMXPath;
 
 $doc = new DOMDocument();
-$doc->loadHTML(Db::forTablePrefix('articles')->where('alias', 'quick-start-1001')->findOne()->text);
+$doc->loadHTML(Db::forTablePrefix('articles')->where('alias', 'quick-start-1001')->findOne()->text, LIBXML_HTML_NODEFDTD | LIBXML_HTML_NOIMPLIED);
 $xpath = new DOMXPath($doc);
 
 /* process nodes */

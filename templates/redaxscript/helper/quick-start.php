0752e2f6c59ba252a41412320b352fd7375704bb
redaxmedia <info@redaxmedia.com>
2020-02-15 13:59:17 +0100

    Switch back to CSS solution to hide link on homepage

--- templates/redaxscript/helper/quick-start.php
+++ templates/redaxscript/helper/quick-start.php
@@ -1,17 +1,4 @@
 <?php
 namespace Redaxscript;
 
-use DOMDocument;
-use DOMXPath;
-
-$doc = new DOMDocument();
-$doc->loadHTML(Db::forTablePrefix('articles')->where('alias', 'quick-start-1001')->findOne()->text, LIBXML_HTML_NODEFDTD | LIBXML_HTML_NOIMPLIED);
-$xpath = new DOMXPath($doc);
-
-/* process nodes */
-
-foreach ($xpath->query('//a') as $node)
-{
-	$node->parentNode->removeChild($node);
-}
-return $doc->saveHTML();
+return Db::forTablePrefix('articles')->where('alias', 'quick-start-1001')->findOne()->text;

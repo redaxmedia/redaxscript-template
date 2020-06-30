a135c0cd553785bc8088efdbf8d9e1b3a56cafe3
redaxmedia <info@redaxmedia.com>
2020-06-30 16:28:10 +0200

    Improve query in CSS and finally remove anchor via quick start helper

--- templates/redaxscript/helper/quick-start.php
+++ templates/redaxscript/helper/quick-start.php
@@ -1,4 +1,12 @@
 <?php
 namespace Redaxscript;
 
-return Db::forTablePrefix('articles')->where('alias', 'quick-start-1001')->findOne()->text;
+use DOMDocument;
+
+$doc = new DOMDocument();
+$doc->loadHTML(Db::forTablePrefix('articles')->where('alias', 'quick-start-1001')->findOne()->text);
+foreach ($doc->getElementsByTagName('a') as $element)
+{
+	$element->parentNode->removeChild($element);
+}
+return $doc->saveHTML();

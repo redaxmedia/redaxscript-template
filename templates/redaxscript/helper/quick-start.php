ed32ded4d85ec656174b18c1f3e2a1fb6f15de1a
redaxmedia <info@redaxmedia.com>
2020-07-07 15:12:43 +0200

    Remove doctype from parsed HTML

--- templates/redaxscript/helper/quick-start.php
+++ templates/redaxscript/helper/quick-start.php
@@ -4,7 +4,7 @@ namespace Redaxscript;
 use DOMDocument;
 
 $doc = new DOMDocument();
-$doc->loadHTML(Db::forTablePrefix('articles')->where('alias', 'quick-start-1001')->findOne()->text, LIBXML_HTML_NODEFDTD | LIBXML_HTML_NOIMPLIED);
+$doc->loadHTML(Db::forTablePrefix('articles')->where('alias', 'quick-start-1001')->findOne()->text, LIBXML_HTML_NODEFDTD);
 foreach ($doc->getElementsByTagName('a') as $element)
 {
 	$element->parentNode->removeChild($element);
